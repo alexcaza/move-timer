753dafd0f3a039464fd4a731dc4dfb0dfb8adcc6
Alex Caza <alex@alexcaza.com>
2021-12-22 16:54:09 -0500

    Get display working in popup
    Also start work on pause logic

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const { menubar } = require("menubar");
-const { app, Tray } = require("electron");
+const { app, BrowserWindow, Tray, ipcMain } = require("electron");
 
-const SIT = 1200;
-const STAND = 480;
-const WALK = 120;
+let intervalRef;
+
+const pad = (number) => (number < 10 ? `0${number}` : number);
+
+const startTimer = (tray, mb) => {
+  const SIT = 1200;
+  const STAND = 480;
+  const WALK = 120;
+  const intervals = [SIT, STAND, WALK];
+  const phases = ["sit", "stand", "walk"];
+
+  let position = 0;
+  let start = Date.now();
+  let time = intervals[position];
+
+  return setInterval(() => {
+    let currentTimeFormatted;
+    const delta = Date.now() - start; // milliseconds elapsed since start
+    const timeNow = Math.floor(delta / 1000); // in seconds
+    time = intervals[position] - timeNow;
+
+    const minutes = pad(Math.floor(time / 60));
+    const seconds = pad(time % 60);
+    currentTimeFormatted = `${minutes}:${seconds}`;
+    console.log("Current time formatted: ", currentTimeFormatted);
+
+    if (time < 1) {
+      position + 1 < intervals.length ? position++ : (position = 0);
+      time = intervals[position];
+      start = Date.now();
+    }
+
+    tray.setTitle(currentTimeFormatted);
+    if (mb && mb.window && mb.window.webContents) {
+      mb.window.webContents.send("timer-updated", {
+        time: currentTimeFormatted,
+        phase: phases[position],
+      });
+    }
+  }, 1000);
+};
 
 app.whenReady().then(() => {
   const tray = new Tray(process.cwd() + "/clock-20px.png");
@@ -12,26 +50,23 @@ app.whenReady().then(() => {
     tooltip: "20 minutes",
     icon: "clock-20px.png",
     tray,
+    browserWindow: {
+      webPreferences: { nodeIntegration: true, contextIsolation: false },
+    },
   });
 
-  mb.on("ready", () => {
-    const start = Date.now();
-    const intervals = [SIT, STAND, WALK];
-    let position = 0;
-    let currentDelta = intervals[position];
+  mb.setOption("webPreferences.nodeIntegration", true);
 
-    setInterval(function () {
-      const delta = Date.now() - start; // milliseconds elapsed since start
-      // total seconds
-      const timeNow = Math.floor(delta / 1000); // in seconds
-      // minutes (currentDelta / 60)
-      currentDelta = intervals[position] - timeNow;
-      tray.setTitle(
-        // TODO: Add seconds to this when brain working
-        `${Math.floor(currentDelta / 60)}`
-      );
-      // alternatively just show wall clock time:
-      // output(new Date().toUTCString());
-    }, 1000);
+  mb.on("ready", () => {});
+
+  intervalRef = startTimer(tray, mb);
+
+  ipcMain.on("TOGGLE_TIMER", (_, data) => {
+    if (data === "paused") {
+      // TODO: Store previous time somewhere to start countdown back up correctly
+      clearInterval(intervalRef);
+    } else {
+      intervalRef = startTimer(tray, mb);
+    }
   });
 });
